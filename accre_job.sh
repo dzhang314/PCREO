e3ed38476c821b5ac6738a956f81cc25c37203cc
David K. Zhang <dzhang314@gmail.com>
2017-07-09 21:23:52 -0500

    Added accre_job.sh variables and repeated runs

--- accre_job.sh
+++ accre_job.sh
@@ -9,12 +9,23 @@
 
 module load Anaconda3
 
+PARAM_S=2.0
+PARAM_D=3
+PARAM_N=500
+
 mkdir /gpfs23/scratch/zhangdk/pcreo_runs/output_data_${SLURM_JOBID}
 cd /gpfs23/scratch/zhangdk/pcreo_runs/output_data_${SLURM_JOBID}
-/home/zhangdk/pcreo/compile.py ./pcreo_sphere_exe /home/zhangdk/pcreo/src/pcreo_sphere.f90 PCREO_DOUBLE_PREC PCREO_PARAM_S=2.0_rk PCREO_PARAM_D=3 PCREO_PARAM_N=500
+/home/zhangdk/pcreo/compile.py ./pcreo_sphere_exe /home/zhangdk/pcreo/src/pcreo_sphere.f90 \
+PCREO_DOUBLE_PREC PCREO_PARAM_S=${PARAM_S}_rk PCREO_PARAM_D=${PARAM_D} PCREO_PARAM_N=${PARAM_N}
 ./pcreo_sphere_exe
 rm ./pcreo_sphere_exe
 /home/zhangdk/pcreo/out2in.py
-/home/zhangdk/pcreo/compile.py ./pcreo_sphere_exe /home/zhangdk/pcreo/src/pcreo_sphere.f90 PCREO_QUAD_PREC PCREO_PARAM_S=2.0_rk PCREO_PARAM_D=3 PCREO_PARAM_N=500
+/home/zhangdk/pcreo/compile.py ./pcreo_sphere_exe /home/zhangdk/pcreo/src/pcreo_sphere.f90 \
+PCREO_QUAD_PREC PCREO_PARAM_S=${PARAM_S}_rk PCREO_PARAM_D=${PARAM_D} PCREO_PARAM_N=${PARAM_N}
 ./pcreo_sphere_exe
+/home/zhangdk/pcreo/out2in.py
+./pcreo_sphere_exe
+/home/zhangdk/pcreo/out2in.py
+./pcreo_sphere_exe
+/home/zhangdk/pcreo/out2in.py
 rm ./pcreo_sphere_exe
