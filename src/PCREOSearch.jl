451a71300892997d906a53b382d0ac9e33db174d
David K. Zhang <dzhang314@gmail.com>
2021-01-28 02:53:52 -0800

    add new symmetrized search script

--- src/PCREOSearch.jl
+++ src/PCREOSearch.jl
@@ -1,177 +1,112 @@
-using LinearAlgebra: eigvals!, Symmetric
-using Random: seed!
+using DZOptimization: BFGSOptimizer, normalize_columns!
+using MultiFloats: Float64x2, Float64x3
 using UUIDs: uuid4
 
-using DZOptimization
-using DZOptimization: half, normalize_columns!
-using MultiFloats
-
 push!(LOAD_PATH, @__DIR__)
-using PCREO
+using PCREO: run!, spherical_riesz_gradient_norm,
+    spherical_riesz_hessian_spectral_gap,
+    convex_hull_facets, parallel_facet_distance
+using PCREOSymmetry: POLYHEDRAL_POINT_GROUPS, symmetrized_riesz_functors
 
 
-const F = typeof(riesz_energy)
-const G = typeof(spherical_riesz_gradient!)
-const C = typeof(constrain_sphere!)
-const N = 2
-
-
-function run!(opt::BFGSOptimizer{F,G,C,Float64,N})
-    while !opt.has_converged[]
-        step!(opt)
-    end
-    return opt
-end
-
-function run!(opt::BFGSOptimizer{F,G,C,T,N}) where {T}
-    println(opt.iteration_count[], '\t',
-            opt.current_objective_value[])
-    while !opt.has_converged[]
-        step!(opt)
-        step!(opt)
-        step!(opt)
-        step!(opt)
-        step!(opt)
-        println(opt.iteration_count[], '\t',
-                opt.current_objective_value[])
-    end
-    return opt
-end
-
-function refine(points::Matrix{T}) where {T}
-    return run!(BFGSOptimizer(riesz_energy, spherical_riesz_gradient!,
-                              constrain_sphere!, points, T(0.01)))
-end
-
-function refine(::Type{T}, opt::BFGSOptimizer{F,G,C,U,N}) where {T,U}
-    return run!(BFGSOptimizer(T, opt))
-end
-
-function generate_point_configuration(dimension::Int, num_points::Int)
-    initial_points = normalize_columns!(randn(dimension, num_points))
-    opt1 = refine(initial_points)
-    opt2 = refine(Float64x2, opt1)
-    opt3 = refine(Float64x3, opt2)
-    return (Float64(opt3.current_objective_value[]),
-            Float64.(opt3.current_point), initial_points)
+function constrain_sphere!(points)
+    normalize_columns!(points)
+    return true
 end
 
 
-spherical_riesz_gradient_norm(points) =
-    maximum(abs.(spherical_riesz_gradient(points)))
+function generate_and_save_configuration(
+        group::Function, orbits::Vector{Function}, num_points::Int)
 
-function symmetrize!(mat::Matrix{T}) where {T}
-    m, n = size(mat)
-    @assert m == n
-    @inbounds for i = 1 : n-1
-        @simd ivdep for j = i+1 : n
-            sym = half(T) * (mat[i, j] + mat[j, i])
-            mat[i, j] = mat[j, i] = sym
+    initial_points = normalize_columns!(randn(3, num_points))
+
+    f1, g1! = symmetrized_riesz_functors(Float64, group, orbits)
+    opt1 = BFGSOptimizer(f1, g1!, constrain_sphere!, initial_points, 1.0e-6)
+    run!(opt1)
+
+    f2, g2! = symmetrized_riesz_functors(Float64x2, group, orbits)
+    opt2 = BFGSOptimizer(Float64x2, f2, g2!, constrain_sphere!, opt1)
+    run!(opt2)
+
+    f3, g3! = symmetrized_riesz_functors(Float64x3, group, orbits)
+    opt3 = BFGSOptimizer(Float64x3, f3, g3!, constrain_sphere!, opt2)
+    run!(opt3)
+
+    num_full_points = (num_points * length(f3.group) +
+                       length(f3.external_points))
+    full_points = Matrix{Float64x3}(undef, 3, num_full_points)
+    k = 0
+    for g in f3.group
+        for col in eachcol(opt3.current_point)
+            full_points[:,k+=1] .= g * col
         end
     end
-    return mat
-end
-
-function spherical_riesz_hessian_spectral_gap(points)
-    dim, num_points = size(points)
-    hess = symmetrize!(spherical_riesz_hessian(points))
-    vals = eigvals!(hess)
-    num_expected_zeros = div(dim * (dim - 1), 2) + num_points
-    expected_zero_vals = vals[1:num_expected_zeros]
-    expected_nonzero_vals = vals[num_expected_zeros+1:end]
-    @assert all(!signbit, expected_nonzero_vals)
-    return (maximum(abs.(expected_zero_vals)) /
-            minimum(expected_nonzero_vals))
-end
-
-
-const QCONVEX_PATH = "C:\\Programs\\qhull-2020.2\\bin\\qconvex.exe"
-
-function convex_hull_facets(points)
-    dim, num_points = size(points)
-    buffer = IOBuffer()
-    process = open(`$QCONVEX_PATH i`, buffer, write=true)
-    println(process, dim)
-    println(process, num_points)
-    for j = 1 : num_points
-        for i = 1 : dim
-            print(process, ' ', points[i,j])
-        end
-        println(process)
+    for point in f3.external_points
+        full_points[:,k+=1] .= point
     end
-    close(process)
-    while process_running(process)
-        sleep(0.001)
-    end
-    first = true
-    num_facets = 0
-    result = Vector{Int}[]
-    seek(buffer, 0)
-    for line in eachline(buffer)
-        if first
-            num_facets = parse(Int, line)
-            first = false
-        else
-            push!(result, [parse(Int, s) + 1 for s in split(line)])
+    @assert k == num_full_points
+
+    c2 = spherical_riesz_hessian_spectral_gap(full_points)
+    if c2 < 1.0
+        c1 = Float64(spherical_riesz_gradient_norm(full_points))
+        facets = convex_hull_facets(Float64.(full_points))
+        c3 = Float64(parallel_facet_distance(full_points, facets))
+        prefix = "PCREO-03-$(lpad(num_full_points, 4, '0'))"
+        filename = "$prefix-$(uuid4()).csv"
+        open(joinpath("D:\\Data\\PCREOSymmetry", filename), "w+") do io
+            println(io, 3)
+            println(io, num_full_points)
+            println(io, opt3.current_objective_value[])
+            println(io, '"', group, '"')
+            if !isempty(orbits)
+                println(io, join(['"' * string(f) * '"'
+                                  for f in orbits], ", "))
+            end
+            println(io, c1)
+            println(io, c2)
+            println(io, c3)
+            println(io)
+            for col in eachcol(full_points)
+                println(io, join(string.(col), ", "))
+            end
+            println(io)
+            for facet in facets
+                println(io, join(string.(facet), ", "))
+            end
+            println(io)
+            for col in eachcol(initial_points)
+                println(io, join(string.(col), ", "))
+            end
         end
     end
-    @assert num_facets == length(result)
+
+    return nothing
+end
+
+
+function powerset(xs::Vector{T}) where {T}
+    result = [T[]]
+    for x in xs
+        for i = 1 : length(result)
+            push!(result, push!(copy(result[i]), x))
+        end
+    end
     return result
-end
-
-
-function generate_and_save_point_configuration(dimension::Int, num_points::Int)
-    energy, points, initial = generate_point_configuration(
-        dimension, num_points)
-    grad_norm = spherical_riesz_gradient_norm(points)
-    spectral_gap = spherical_riesz_hessian_spectral_gap(points)
-    success = (grad_norm < 1.0e-10) && (spectral_gap < 1.0e-10)
-    facets = convex_hull_facets(points)
-    open("$(ifelse(success, "PCREO", "FAIL"))-$(lpad(dimension, 2, '0'))-" *
-         "$(lpad(num_points, 4, '0'))-$(uuid4()).csv", "w+") do io
-        println(io, dimension)
-        println(io, num_points)
-        println(io, energy)
-        println(io)
-        for j = 1 : num_points
-            for i = 1 : dimension
-                if i > 1
-                    print(io, ", ")
-                end
-                print(io, points[i,j])
-            end
-            println(io)
-        end
-        println(io)
-        for facet in facets
-            for i = 1 : length(facet)
-                if i > 1
-                    print(io, ", ")
-                end
-                print(io, facet[i])
-            end
-            println(io)
-        end
-        println(io)
-        for j = 1 : num_points
-            for i = 1 : dimension
-                if i > 1
-                    print(io, ", ")
-                end
-                print(io, initial[i,j])
-            end
-            println(io)
-        end
-    end
 end
 
 
 function main()
     while true
-        for num_points = 50 : 199
-            generate_and_save_point_configuration(3, num_points)
+        for num_points = 1 : 100
+            for (group_function, orbit_functions) in POLYHEDRAL_POINT_GROUPS
+                for selected_orbits in powerset(orbit_functions)
+                    generate_and_save_configuration(
+                        group_function, selected_orbits, num_points)
+                end
+            end
         end
     end
 end
+
 
 main()
